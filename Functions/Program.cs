34652e6a241fb7a497135025b9420ff9396823cd
nirajandhungel <subashdhungel555@gmail.com>
2025-08-03 23:08:47 +0545

    Create a function that takes arguments and retur values

---
+++ Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Functions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int checkNum = 5;
+            Console.WriteLine(IsEven(checkNum));
+        }
+
+        // function with args and return value
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
