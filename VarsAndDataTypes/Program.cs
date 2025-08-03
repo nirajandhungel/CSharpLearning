62bc78e70c95a6abe3601fe3e17b2ebb44ad46f3
nirajandhungel <subashdhungel555@gmail.com>
2025-08-03 22:58:48 +0545

    Vars , Data Types and 3 types of priting vars

---
+++ VarsAndDataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace VarsAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 19;
+            string name = "Nirajan";
+            bool isStudnet = true;
+            double height = 5.7;
+            char grade = 'A';
+            // First : Displaying using +
+            Console.WriteLine("Name : " + name + ", is student : " + isStudnet + ", Age: " + age + ", Height : " + height + ", Grade : " + grade);
+
+            // Second : Prinitng using ${} , String Interpolation
+            Console.WriteLine($" Name : {name}, Age : {age}, height: {height}, is student : {isStudnet}, Grade: {grade}");
+
+            // Third : format string {0},{1}
+            Console.WriteLine("Name :{0}, age : {1}, is student : {2}, Height :{3}, Grade : {3}",name,age,isStudnet,height,grade);
+        }
+    }
+}
