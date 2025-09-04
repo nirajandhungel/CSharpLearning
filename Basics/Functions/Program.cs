ad9faddfd7459e917cc4602b533fd3a3bb57f861
nirajandhungel <subashdhungel555@gmail.com>
2025-09-04 14:28:25 +0545

    add(functions): basic function in dotnet

---
+++ Basics/Functions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Functions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int checkNum = 5;
+            IsOdd(3);
+            Console.WriteLine(IsEven(checkNum));
+        }
+
+        // function with args and return value
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        // function with args and no return value
+        public static void IsOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"No, {num} is not an odd number");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, {num} is an odd number");
+
+            }
+        }
+    }
+}
