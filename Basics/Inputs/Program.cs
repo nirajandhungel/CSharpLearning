4a8ace54c91a85a8a85d10924c08cfadd9759a39
nirajandhungel <subashdhungel555@gmail.com>
2025-09-04 21:41:30 +0545

    add: learn to take input

---
+++ Basics/Inputs/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Program{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number : ");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"You entered : {num}");
+    }
+}
